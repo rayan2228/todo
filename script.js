47fd17297b7e8f468436dce92cc07f490568b662
rayan2228 <rayanh.dev@gmail.com>
2025-01-07 16:33:53 +0600

    add task on press enter button

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 let editId = null;
 const addEventListeners = () => {
     addBtnElm.addEventListener('click', addTask);
-    inputElm.addEventListener('input', inputChanges)
+    inputElm.addEventListener('keydown', inputChanges)
     searchElm.addEventListener('input', searchTask);
 }
 
-const inputChanges = () => {
+const inputChanges = (e) => {
+    if (e.key === 'Enter') {
+        addTask();
+    }
     messageElm.textContent = '';
 }
 
